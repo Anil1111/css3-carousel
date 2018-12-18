6cf653d0793e92edd8ff15a6a5ff22098bf6fee4
Lukas Abegg <lab@xcentric.ch>
2018-12-18 14:05:09 +0100

    Typo

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function(){
 	
 	var slider = $('.perspective-slider');
 	slider.on('change', function() {
-		Indigio.css3Carousel.createCSS3(".carousel", 'perspective-origin-y', this.value+'%');
+		Indigio.Css3Carousel.createCSS3(".carousel", 'perspective-origin-y', this.value+'%');
 		console.log("Slider Value:" + this.value + "%");
 	});
 });
